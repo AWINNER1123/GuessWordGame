82c0693ed292e3bbba4bc81d6351e7bf4b78dc73
AWINNER1123 <92835131+AWINNER1123@users.noreply.github.com>
2022-07-12 18:38:02 -0700

    Add files via upload

---
+++ activity2.js
@@ -0,0 +1,9 @@
+function getScore() {
+    score = localStorage.getItem("score");
+    document.getElementById("update").innerHTML = "<h1>Score: " + score + "</h1>";
+}
+
+function goBack() {
+
+    window.location = "activity_1.html";
+}
